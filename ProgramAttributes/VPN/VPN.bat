c3d98e51dfbd444921da575cae8900ad7002b241
Ryan <ryanyoungjohn321@gmail.com>
2023-05-16 21:55:14 -0400

    VPN processing

--- ProgramAttributes/VPN/VPN.bat
+++ ProgramAttributes/VPN/VPN.bat
@@ -2,10 +2,14 @@
 chcp 65001 >nul
 cls
 
-if exist "C:\Program Files (x86)\Hotspot Shield" (
-    "C:\Program Files (x86)\Hotspot Shield\12.1.1\bin\hsscp.exe"
+if exist "C:\Program Files (x86)\Hotspot Shield\12.1.1" (
+    goto VPN
 ) else (
     start C:\CobraClient\CobraClientFiles\VPN\installer.exe&&echo.&&echo When complete click any key&&pause
 )
 
 call C:\CobraClient\CobraClientFiles\ProgramMenus\HomeMenu.bat
+
+:VPN
+echo You can close this window.
+"C:\Program Files (x86)\Hotspot Shield\12.1.1\bin\hsscp.exe"
