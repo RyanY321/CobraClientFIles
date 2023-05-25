cd2a71b155eb238a67a5cf7205df3e8198a8f4af
Ryan <ryanyoungjohn321@gmail.com>
2023-05-24 20:02:02 -0400

    Home Fix

--- ProgramMenus/General/HomeMenu2.bat
+++ ProgramMenus/General/HomeMenu2.bat
@@ -3,7 +3,7 @@
 chcp 65001 >nul
 cls
 
-title Cobra Client
+title Cobra Client - Page 2
 echo.
 echo ░█████╗░░█████╗░██████╗░██████╗░░█████╗░
 echo ██╔══██╗██╔══██╗██╔══██╗██╔══██╗██╔══██╗
@@ -25,29 +25,15 @@ echo Version - %version%
 echo Press O to open Options
 echo Press E to exit
 echo ==========================================
-echo (1) Drivers
+echo (1) Shutdown Options
 echo ==========================================
-echo (2) Diskpart
+echo (2) Suggestions
 echo ==========================================
-echo (3) Windows Tools
+echo (3) Last Page
 echo ==========================================
-echo (4) Ip Tools
-echo ==========================================
-echo (5) Logs
-echo ==========================================
-echo (6) Install Tron -Malware Remover-
-echo ==========================================
-echo (7) Next Page
-echo ==========================================
-choice /c 1234567oe /n
-if %errorlevel% == 1 call C:\CobraClient\CobraClientFiles\ProgramAttributes\General\DriversQuery.bat
-if %errorlevel% == 2 diskpart
-if %errorlevel% == 3 call C:\CobraClient\CobraClientFiles\ProgramMenus\WindowsTools\WindowsMenu.bat
-if %errorlevel% == 4 call C:\CobraClient\CobraClientFiles\ProgramMenus\IPTools\IPToolsMenu.bat
-if %errorlevel% == 5 call C:\CobraClient\CobraClientFiles\ProgramMenus\Logs\LogsMenu.bat
-if %errorlevel% == 6 call C:\CobraClient\CobraClientFiles\ProgramMenus\Tron\TronMenu.bat
-if %errorlevel% == 7 call C:\CobraClient\CobraClientFile\ProgramMenus\General\HomeMenu2.bat
-if %errorlevel% == 8 call C:\CobraClient\CobraClientFiles\ProgramMenus\Options\OptionsMenu.bat
-if %errorlevel% == 9 exit
-
-goto Top
+choice /c 123oe /n
+if %errorlevel% == 1 call C:\CobraClient\CobraClientFiles\ProgramMenus\Shutdown\ShutdownMenu.bat
+if %errorlevel% == 2 call C:\CobraClient\CobraClientFiles\ProgramAttributes\General\Suggestion.bat
+if %errorlevel% == 3 call C:\CobraClient\CobraClientFiles\ProgramMenus\HomeMenu.bat
+if %errorlevel% == 4 call C:\CobraClient\CobraClientFiles\ProgramMenus\Options\OptionsMenu.bat
+if %errorlevel% == 5 exit
