85b0bab819e45ec53f2d2083daeb2426e46b64ec
Ryan <ryanyoungjohn321@gmail.com>
2023-05-24 20:32:13 -0400

    Update Updater.bat

--- ProgramAttributes/Options/Updater.bat
+++ ProgramAttributes/Options/Updater.bat
@@ -3,14 +3,22 @@ chcp 65001 >nul
 cls
 
 if exist "C:\CobraClientUpdater.exe" (
+    pause
     del /F /Q C:\CobraClientUpdater.exe
+    pause
     goto Continue
+    pause
 ) else (
+    pause
     goto Continue
+    pause
 )
 
 :Continue
 move /Y C:\CobraClient\CobraClientFiles\CobraClientUpdater.exe C:\
+pause
 timeout 3 >nul
+pause
 start C:\CobraClientUpdater.exe
+pause
 exit
