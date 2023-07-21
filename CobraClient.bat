347fb2b6167f35c1a2c879f254a97dfd8a037e53
Ryan <ryanyoungjohn321@gmail.com>
2023-07-20 20:01:28 -0400

    Update CobraClient.bat

--- CobraClient.bat
+++ CobraClient.bat
@@ -4,6 +4,7 @@ color 04
 set version=2.8.5
 set BN=BetaCC071923_31753
 set InstallationChannel=Beta Release
+mode con cols=125 lines=30
 cls 
 
 call C:\CobraClient\CobraClientFiles\Resources\ColorCheck.bat
