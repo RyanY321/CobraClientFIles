b5dd39b11aaded247527b3e389d20b26776e6e27
Ryan <ryanyoungjohn321@gmail.com>
2023-07-24 17:47:08 -0400

    Beta 2.9

--- CobraClient.bat
+++ CobraClient.bat
@@ -1,8 +1,8 @@
 @echo off
 chcp 65001 >nul
 color 04
-set version=2.8.5
-set BN=BetaCC071923_31753
+set version=2.90
+set BN=BetaCC072423_23734
 set InstallationChannel=Beta Release
 mode con cols=125 lines=30
 cls 
