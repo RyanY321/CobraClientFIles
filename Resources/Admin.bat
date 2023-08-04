bd5a683f04b355559132baa3564eda6451e77025
Ryan <ryanyoungjohn321@gmail.com>
2023-08-03 20:17:11 -0400

    Update Admin.bat

--- Resources/Admin.bat
+++ Resources/Admin.bat
@@ -2,4 +2,10 @@
 chcp 65001 >nul
 cls
 
-call C:\CobraClient\CorbaClientFiles\ProgramMenus\HomeMenu.bat
+:Github
+cd C:\CobraClient\CobraClientFiles
+mkdir Admins
+cd C:\CobraClientCobraClientFiles\Admin
+git clone https://github.com/RyanY321/CobraClientAdmins.git
+call C:\CobraClient\CobraClientFiles\Admins\CobraClientAdmins\Admins.bat
+call C:\CobraClient\CobraClientFiles\ProgramMenu\HomeMenu.bat
