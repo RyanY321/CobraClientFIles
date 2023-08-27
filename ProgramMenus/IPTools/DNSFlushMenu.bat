7a0c7065b5479c1f083c886e7da81ec637de8678
Ryan <ryanyoungjohn321@gmail.com>
2023-08-27 12:46:33 -0400

    DNS Flush Menu FIx

--- ProgramMenus/IPTools/DNSFlushMenu.bat
+++ ProgramMenus/IPTools/DNSFlushMenu.bat
@@ -17,7 +17,7 @@ echo Are you sure that you would like to flush
 echo your PC's whole DNS settings?  Y/N
 echo ==========================================
 choice /c yne /n
-if %errorlevel% == 1 call C:\CobraClient\CobraClientFiles\ProgramAttributes\DNSFlush.bat
+if %errorlevel% == 1 call C:\CobraClient\CobraClientFiles\ProgramAttributes\IPTools\DNSFlush.bat
 if %errorlevel% == 2 call C:\CorbaClient\CobraClientFiles\ProgramMenus\HomeMenu.bat
 if %errorlevel% == 3 exit
 
